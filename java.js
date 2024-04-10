2880ec0f39745199fef65168a4114fd674ebd85f
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-04-10 18:37:31 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,11 @@
+Video.addEventListener("click", function video(){
+    alert("ini video Glow Like Dat");
+});
+
+Audio.addEventListener("click", function video(){
+    alert("ini audio Glow Like Dat");
+});
+
+Gambar.addEventListener("click", function video(){
+    alert("ini gambar Glow Like Dat");
+});
